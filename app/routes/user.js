3878779287ae2c7352c5ce954e9abbe25b218bf6
vikas1990kumar <kumar1990vikas@gmail.com>
2019-09-13 21:33:32 +0530

    my-seventh-commit

--- app/routes/user.js
+++ app/routes/user.js
@@ -9,27 +9,27 @@ module.exports.setRouter = (app) => {
     let baseUrl = `${appConfig.apiVersion}/users`;
 
 
-    app.get(`${baseUrl}/all`,  userController.getAllIssue);
+    app.get(`${baseUrl}/all`, auth.isAuthorized, userController.getAllIssue);
 
 
     // params: userId.
    // app.get(`${baseUrl}/:userId/details`, auth.isAuthorized, userController.getSingleUser);
 
-   app.get(`${baseUrl}/:receiverName/details`,  userController.getSingleUser);
+   app.get(`${baseUrl}/:receiverName/details`, auth.isAuthorized, userController.getSingleUser);
 
-   app.get(`${baseUrl}/:issueId`, userController.getSingleIssue);
+   app.get(`${baseUrl}/:issueId`, auth.isAuthorized, userController.getSingleIssue);
 
-   app.get(`${baseUrl}/:issueData/search`, userController.getSingleSearch);
+   app.get(`${baseUrl}/:issueData/search`, auth.isAuthorized, userController.getSingleSearch);
 
-   app.get(`${baseUrl}/:issueId/commentDetails`, userController.getComments);
+   app.get(`${baseUrl}/:issueId/commentDetails`, auth.isAuthorized, userController.getComments);
 
    
     // params: firstName, lastName, email, mobileNumber, password, apiKey.
-    app.post(`${baseUrl}/signup`, userController.signUpFunction);
+    app.post(`${baseUrl}/signup`, auth.isAuthorized, userController.signUpFunction);
 
-    app.post(`${baseUrl}/create`, userController.createIssue);
+    app.post(`${baseUrl}/create`, auth.isAuthorized, userController.createIssue);
 
-    app.post(`${baseUrl}/comment`, userController.createComment);
+    app.post(`${baseUrl}/comment`, auth.isAuthorized, userController.createComment);
 
     
 
@@ -38,9 +38,9 @@ module.exports.setRouter = (app) => {
 
    
 
-    app.post(`${baseUrl}/login`, userController.loginFunction);
+    app.post(`${baseUrl}/login`, auth.isAuthorized, userController.loginFunction);
 
-    app.put(`${baseUrl}/:issueId/edit`,  userController.editIssue);
+    app.put(`${baseUrl}/:issueId/edit`, auth.isAuthorized, userController.editIssue);
 
     app.post(`${baseUrl}/:userId/delete`, auth.isAuthorized, userController.deleteUser);
 
--- config/appConfig.js
+++ config/appConfig.js
@@ -1,6 +1,6 @@
 let appConfig = {};
 
-appConfig.port = 3000;
+appConfig.port = 3001;
 appConfig.allowedCorsOrigin = "*";
 appConfig.env = "dev";
 appConfig.db = {
